f315f5a401e20ea817772977d05c794595482e21
devch96 <chjung1006@gmail.com>
2023-08-09 17:05:08 +0900

    [Bronze III] Title: 세탁소 사장 동혁

--- Algorithm/Greedy/2720 세탁소 사장 동혁/BJ_2720_세탁소_사장_동혁.java
+++ Algorithm/Greedy/2720 세탁소 사장 동혁/BJ_2720_세탁소_사장_동혁.java
@@ -1,5 +1,31 @@
+import java.io.*;
+
 public class BJ_2720_세탁소_사장_동혁 {
-    public static void main(String[] args) {
-        System.out.println("helloWorld");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int change = Integer.parseInt(br.readLine());
+            bw.write(calculateCoinCount(change).toString());
+            bw.newLine();
+            bw.flush();
+        }
+    }
+
+    private static StringBuilder calculateCoinCount(int change){
+        StringBuilder sb = new StringBuilder();
+        int quarter = 25;
+        int dime = 10;
+        int nickel = 5;
+        int penny = 1;
+        sb.append(change/quarter+" ");
+        change %= quarter;
+        sb.append(change/dime+" ");
+        change %= dime;
+        sb.append(change/nickel+" ");
+        change %= nickel;
+        sb.append(change/penny);
+        return sb;
     }
 }
