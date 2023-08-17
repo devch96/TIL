0ad8c3748416176f0c8997159ed0b46531816e09
devch96 <chjung1006@gmail.com>
2023-08-17 14:41:25 +0900

    [GOLD IV] Title: 최소 스패닝 트리

---
+++ Algorithm/그래프/1197 최소 스패닝 트리/BJ_1197_최소_스패닝_트리.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_1197_최소_스패닝_트리 {
+    static int[] parent;
+    static class Edge implements Comparable<Edge>{
+        int s, e, v;
+
+        public Edge(int s, int e, int v) {
+            this.s = s;
+            this.e = e;
+            this.v = v;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.v - o.v;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Queue<Edge> queue = new PriorityQueue<>();
+        parent = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            queue.offer(new Edge(s, e, v));
+        }
+        int useEdge = 0;
+        int result = 0;
+        while (useEdge < n - 1) {
+            Edge now = queue.poll();
+            if (find(now.s) != find(now.e)) {
+                union(now.s, now.e);
+                result += now.v;
+                useEdge++;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static void union(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a != b){
+            parent[b] = a;
+        }
+    }
+
+    private static int find(int a){
+        if (a == parent[a]) {
+            return a;
+        }else{
+            return parent[a] = find(parent[a]);
+        }
+    }
+}
