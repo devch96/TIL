ba3a97078c94661734f187488a7370522d825c67
devch96 <chjung1006@gmail.com>
2023-08-18 11:12:06 +0900

    [SILVER I] Title: 쉬운 계단 수

---
+++ Algorithm/동적 계획법/10844 쉬운 계단 수/BJ_10844_쉬운_계단_수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_10844_쉬운_계단_수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n + 1][11];
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i-1][1];
+            dp[i][9] = dp[i-1][8];
+            for (int j = 1; j <= 8; j++) {
+                dp[i][j] = (dp[i-1][j-1] + dp[i-1][j+1]) % 1_000_000_000;
+            }
+        }
+        long sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = (sum + dp[n][i]) % 1_000_000_000;
+        }
+        System.out.println(sum);
+    }
+}
