1cad8f81cd7eba43624fe189c8a5a40ced09c52c
devch96 <chjung1006@gmail.com>
2023-08-31 15:27:17 +0900

    팩토리 메서드 패턴(Factory Method Pattern)

--- OOP/DesignPattern/생성 패턴/팩토리 메서드 패턴/FactoryMethodDemo.java
+++ OOP/DesignPattern/생성 패턴/팩토리 메서드 패턴/FactoryMethodDemo.java
@@ -1,7 +1,7 @@
 import factory.NYPizzaStore;
 import factory.PizzaStore;
 
-public class Demo {
+public class FactoryMethodDemo {
     public static void main(String[] args) {
         PizzaStore pizzaStore = new NYPizzaStore();
         pizzaStore.orderPizza("cheese");
