03c289e3ac8064f5e3800ae9d1f906022c5544ad
devch96 <chjung1006@gmail.com>
2023-10-19 10:23:20 +0900

    [SILVER III] Title: 이친수

---
+++ Algorithm/동적 계획법/2193 이친수/BJ_2193_이친수.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_2193_이친수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long[][] dp = new long[91][2];
+        dp[1][1] = 1;
+        dp[2][0] = 1;
+        dp[3][1] = 1;
+        dp[3][0] = 1;
+        for (int i = 4; i <= 90; i++) {
+            dp[i][0] = dp[i-1][0] + dp[i-1][1];
+            dp[i][1] = dp[i-1][0];
+        }
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(dp[n][0] + dp[n][1]);
+    }
+}
