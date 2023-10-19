0f018bcfa246a86e6a16d005eafaf8ae03e45eb5
devch96 <chjung1006@gmail.com>
2023-10-19 10:10:25 +0900

    [SILVER II] Title: 가장 긴 감소하는 부분 수열

---
+++ Algorithm/동적 계획법/11722 가장 긴 감소하는 부분 수열/BJ_11722_가장_긴_감소하는_부분_수열.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_11722_가장_긴_감소하는_부분_수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j]+1;
+                }
+            }
+        }
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
