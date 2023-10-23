459caafbb7f577119be104b0a2bf74e68816357f
devch96 <chjung1006@gmail.com>
2023-10-23 10:06:17 +0900

    [GOLD V] Title: 합분해

--- Algorithm/동적 계획법/2225 합분해/BJ_2225_합분해.java
+++ Algorithm/동적 계획법/2225 합분해/BJ_2225_합분해.java
@@ -7,18 +7,19 @@ public class BJ_2225_합분해 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        final long mod = 1_000_000_000;
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[201][201];
-        dp[1][1] = 1;
-        for (int i = 1; i < 201; i++) {
+        long[][] dp = new long[201][201];
+        for (int i = 0; i < 201; i++) {
+            dp[1][i] = i;
+        }
+        for (int i = 2; i < 201; i++) {
             for (int j = 1; j < 201; j++) {
-                for (int l = 1; l < i; l++) {
-                    dp[i][j] += dp[l][j-1];
-                }
-                dp[i][j] += dp[i][j-1]+1;
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % mod;
             }
         }
-        System.out.println(dp[n][k]);
+
+        System.out.println(dp[n][k] % mod);
     }
 }
