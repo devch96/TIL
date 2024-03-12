138a1303fd29704d3af2443539fdab0838a85d75
devch96 <chjung1006@gmail.com>
2024-03-12 11:08:28 +0900

    [Level 3] 숫자 게임

---
+++ Algorithm/정렬/P - 숫자 게임/PG_Lv3_숫자_게임.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class PG_Lv3_숫자_게임 {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int x = -1;
+
+        for(int i=0;i<A.length;i++){
+            for(int j=x+1;j<B.length;j++){
+                if(A[i]<B[j]){
+                    x = j;
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
