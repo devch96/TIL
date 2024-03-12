d9169895afbf5399580d25bfea33775d32713826
devch96 <chjung1006@gmail.com>
2024-03-12 10:53:49 +0900

    [Level 3] 등굣길

--- Algorithm/동적 계획법/P - 등굣길/PG_Lv3_등굣길.java
+++ Algorithm/동적 계획법/P - 등굣길/PG_Lv3_등굣길.java
@@ -3,31 +3,33 @@ public class PG_Lv3_등굣길 {
     public int solution(int m, int n, int[][] puddles) {
         map = new int[n][m];
         map[0][0] = 1;
-        if(puddles[0].length > 0){
-            for(int[] xy : puddles){
-                map[xy[1]-1][xy[0]-1] = -1;
-            }
-        }
-        for(int i = 1 ;i < n; i++){
-            map[i][0] = (map[i][0] == -1 | map[i-1][0] == -1) ? -1 : 1;
-        }
-        for(int j = 1 ;j < m ;j++){
-            map[0][j] = (map[0][j] == -1 | map[0][j-1] == -1) ? -1 : 1;
-        }
-        findPath(m,n);
-        return map[n-1][m-1] == -1 ? 0 : map[n-1][m-1];
+        findPath(m,n,puddles);
+        int answer = map[n-1][m-1];
+        return answer % 1_000_000_007;
     }
 
-    private void findPath(int m, int n) {
-        for(int i = 1; i < n; i++) { // 3
-            for(int j = 1; j < m; j++) { // 4
-                if(map[i][j] != -1) {
-                    int a = map[i-1][j] == -1 ? 0 : map[i-1][j];
-                    int b = map[i][j-1] == -1 ? 0 : map[i][j-1];
-                    map[i][j] = (a+b) == 0 ? -1 : (a+b) % 1000000007;
+    private void findPath(int m, int n, int[][] puddles) {
+        for(int i = 0; i < n; i++) { // 3
+            for(int j = 0; j < m; j++) { // 4
+                if(isNotPuddles(i,j, puddles)) {
+                    if(i != 0) {
+                        map[i][j] += map[i-1][j] % 1_000_000_007;
+                    }
+                    if(j != 0) {
+                        map[i][j] += map[i][j-1] % 1_000_000_007;
+                    }
                 }
             }
         }
+    }
+
+    private boolean isNotPuddles(int x, int y, int[][] puddles) {
+        for(int[] xy : puddles) {
+            if(xy[0] == y+1 && xy[1] == x+1) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
